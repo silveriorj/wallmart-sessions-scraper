10d40ae4185922b760d7dd4c9412b8b6fe236a50
Raul Silverio <rjosesilverio@gmail.com>
2020-10-03 00:35:22 -0300

    getting product links and navigating through the available pages

--- scrapers/spiders/ca_walmart.py
+++ scrapers/spiders/ca_walmart.py
@@ -1,12 +1,23 @@
+import json
 import scrapy
-
+import re
 from scrapers.items import ProductItem
+import logging
 
 
 class CaWalmartSpider(scrapy.Spider):
-    name = "ca_walmart"
-    allowed_domains = ["walmart.ca"]
-    start_urls = ["https://www.walmart.ca/en/grocery/fruits-vegetables/fruits/N-3852"]
+    name = 'ca_walmart'
+    allowed_domains = ['walmart.ca']
+    start_urls = ['https://www.walmart.ca/en/grocery/fruits-vegetables/fruits/N-3852']
+    header = {
+        'Host': 'www.walmart.ca',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Content-Type': 'application/json',
+        'Connection': 'keep-alive'
+    }
 
     def parse(self, response):
         pass
