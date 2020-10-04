0c208497924bd4bf30c0023787c69b641515d2cc
Raul Silverio <rjosesilverio@gmail.com>
2020-10-04 15:25:23 -0300

    handling CSV files for ingestion, cleaning code for maintenance

--- integrations/richart_wholesale_club/ingestion.py
+++ integrations/richart_wholesale_club/ingestion.py
@@ -1,19 +1,77 @@
 import os
-
-import numpy as np
+import sys
+import re
 import pandas as pd
 
-from models import BranchProduct, Product
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+if PROJECT_DIR not in sys.path:
+    sys.path.append(PROJECT_DIR)
 
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 ASSETS_DIR = os.path.join(PROJECT_DIR, "assets")
 PRODUCTS_PATH = os.path.join(ASSETS_DIR, "PRODUCTS.csv")
 PRICES_STOCK_PATH = os.path.join(ASSETS_DIR, "PRICES-STOCK.csv")
+DB_DIR = os.path.join(PROJECT_DIR, 'scrapers', 'db.sqlite')
+
+
+# Styling functions
+def lower_column_names(*args):
+    for df in args:
+        df.columns = [column.lower() for column in df.columns]
+
+
+def capitalize_column_names(df, col_names):
+    for col in col_names:
+        df[col] = df[col].str.capitalize()
+
+
+def lower_column_records(df, col_names):
+    for col in col_names:
+        df[col] = df[col].str.lower()
 
 
 def process_csv_files():
-    products_df = pd.read_csv(filepath_or_buffer=PRODUCTS_PATH, sep="|",)
-    prices_stock_df = pd.read_csv(filepath_or_buffer=PRICES_STOCK_PATH, sep="|",)
+    products_df = pd.read_csv(filepath_or_buffer=PRODUCTS_PATH, sep="|", )
+    prices_stock_df = pd.read_csv(filepath_or_buffer=PRICES_STOCK_PATH, sep="|", )
+
+    # Filtering STOCK > 0 and BRANCHES in [MM, RHSM]
+    prices_stock_df = prices_stock_df[prices_stock_df['STOCK'] > 0]
+    prices_stock_df = prices_stock_df.loc[prices_stock_df['BRANCH'].isin(['MM', 'RHSM'])]
+
+    # Handling null values
+    products_df.fillna(value='', inplace=True)
+
+    # Adding STORE and URL columns
+    products_df['STORE'] = 'Richarts'
+    products_df['URL'] = 'n/a'
+
+    # Filtering products_df present in prices_stock_df
+    query = products_df['SKU'].isin(prices_stock_df['SKU'].values)
+    products_df = products_df[query]
+
+    # Taking the lowest pricing and the total stock availability
+    branchproducts_df = prices_stock_df.groupby(['SKU', 'BRANCH'], as_index=False, sort=False,
+                                                group_keys=False).aggregate({'PRICE': 'min', 'STOCK': 'sum'})
+
+    # Converting data types
+    prices_stock_df['BRANCH'] = prices_stock_df['BRANCH'].astype('category')
+
+    # Cleaning & styling regex
+    products_df['DESCRIPTION'] = products_df['DESCRIPTION'].apply(lambda x: re.sub('<.*?>', '', x))
+    products_df['PACKAGE'] = products_df['DESCRIPTION'].apply(lambda x: re.findall(r'(?!0)\d+\s?\w+\.?$', x))
+    products_df['PACKAGE'] = products_df['PACKAGE'].apply(''.join).apply(lambda x: x.replace('.', ''))
+
+    products_df['CATEGORY'] = products_df['CATEGORY'].str.cat(
+        [products_df['SUB_CATEGORY'], products_df['SUB_SUB_CATEGORY']], sep=' | ')
+
+    # Dropping not required columns
+    products_df = products_df.drop(
+        columns=['SUB_CATEGORY', 'SUB_SUB_CATEGORY', 'ORGANIC_ITEM', 'KIRLAND_ITEM', 'BUY_UNIT', 'FINELINE_NUMBER',
+                 'DESCRIPTION_STATUS'])
+
+    # Using styling functions
+    capitalize_column_names(products_df, ['NAME', 'BRAND', 'DESCRIPTION'])
+    capitalize_column_names(products_df, ['CATEGORY', 'PACKAGE'])
+    lower_column_names(products_df, branchproducts_df)
 
 
 if __name__ == "__main__":
